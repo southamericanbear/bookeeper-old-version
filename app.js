99c7df913dac3563740715fca4e52aa27141bd1d
alan Dittler <dittler.a@gmail.com>
2020-11-03 20:36:19 -0300

    update/view/delete added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const methodOverride = require("method-override");
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
